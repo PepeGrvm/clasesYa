5252fcf808dee1c8039097c78466c3bf8769086d
PepeGrvm <jose.manuel3010@hotmail.com>
2023-11-23 12:20:44 -0300

    rutas

--- resources/views/filtroCursos.blade.php
+++ resources/views/filtroCursos.blade.php
@@ -49,5 +49,31 @@
     @endforeach
 </div>
 
+<!-- Estructura de la modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Pepito Carrion</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Contenido de la modal -->
+                <img src="{{ asset('img/2784445.png') }}" style="width: 200px" class="card-img-top" alt="...">
+                <p style="padding: 10px">Hola, soy un apasionado profesor de matemáticas con una sólida formación académica y experiencia en la enseñanza. Con más de 7 años de experiencia, he trabajado con estudiantes de diversos niveles académicos, desde principiantes hasta avanzados.</p>
 
+                <p style="padding: 10px">Mis horarios son flexibles, y estoy comprometido a trabajar con los estudiantes para encontrar horarios que se adapten a sus necesidades. Por clase $25.000</p>
+                <p style="padding-left: 10px">Metodos de contacto</p>
+                <p style="padding-left: 10px">Fono: +56 9 12345678 <br> Email: Pepito@Carrion.cl <br> Linkedin: @PepitoProfe123</p>
+                <p></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+
+                <a href="perfilProfe"><button type="button" class="btn btn-primary">Contactar</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
